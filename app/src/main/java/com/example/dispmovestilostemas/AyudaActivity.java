b8c51553d5ec7ab1e1b6b4930b10264c291e0344
Julian Lopez <jlopezsa@gmail.com>
2021-09-23 13:07:35 -0500

    Archivo AyudaActivity.java creado

---
+++ app/src/main/java/com/example/dispmovestilostemas/AyudaActivity.java
@@ -0,0 +1,12 @@
+package com.example.dispmovestilostemas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AyudaActivity extends Activity {
+
+    @Override public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_ayuda);
+    }
+}
