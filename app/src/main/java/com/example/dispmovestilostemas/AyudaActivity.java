91d1508537509e5bbeb4e73b6d623ad46f1f9b8e
Julian Lopez <jlopezsa@gmail.com>
2021-09-23 23:53:47 -0500

    Transicion entre actividad principal y nueva actividad creada con retorno

--- app/src/main/java/com/example/dispmovestilostemas/AyudaActivity.java
+++ app/src/main/java/com/example/dispmovestilostemas/AyudaActivity.java
@@ -1,12 +1,32 @@
 package com.example.dispmovestilostemas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AyudaActivity extends Activity {
+
+    private Button boton_regresar;
 
     @Override public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_ayuda);
+
+        boton_regresar = (Button) findViewById(R.id.btn_regresar);
+
+        boton_regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarMainActivity(null);
+            }
+        });
     }
+
+    public void lanzarMainActivity(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/dispmovestilostemas/MainActivity.java
+++ app/src/main/java/com/example/dispmovestilostemas/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.dispmovestilostemas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton_ayuda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton_ayuda = (Button) findViewById(R.id.btn_ayuda);
+
+        boton_ayuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarAyuda(null);
+            }
+        });
+    }
+
+    public void lanzarAyuda(View view){
+        Intent i = new Intent(this,AyudaActivity.class);
+        startActivity(i);
     }
 }
